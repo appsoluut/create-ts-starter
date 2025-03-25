bd2c8cc621e3a699e0da1ca068be3f4d48db1ea0
Paul Hameteman <paul.hameteman@ns.nl>
2025-03-25 23:23:34 +0100

    Make installer work

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,49 @@
 #!/usr/bin/env node
 
-import { intro, text, tasks, outro, isCancel, cancel } from '@clack/prompts';
-const { execSync } = require('child_process');
+import { intro, text, tasks, outro, isCancel, cancel, log } from '@clack/prompts';
+import { execSync } from 'child_process';
+import { promises } from 'fs';
+
+const sum =
+`export function sum(a: number, b: number): number {
+    return a + b;
+}
+`
+
+const sumTest =
+`import { sum } from '../src/index'
+
+describe('sum', () => {
+    it("sum of 1 + 2 should return 3", () => {
+        // arrange
+        const expectedOutput = 3;
+        
+        // act
+        let result = sum(1, 2);
+
+        // assert
+        expect(result).toBe(expectedOutput);
+    })
+});`
+
+const gitignore =
+`node_modules/
+package-lock.json
+`
+
+const jestconfig =
+`module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+};
+`
 
 function onCancel(message: string) {
     cancel(message);
     process.exit(0);
 }
 
-function myExecSync(cmd: string) {
+async function myExecSync(cmd: string) {
     var output;
     try {
         output = execSync(cmd, { stdio: 'pipe' });
@@ -16,6 +51,32 @@ function myExecSync(cmd: string) {
         onCancel(err as string);
     }
     return output;
+}
+
+async function stripComments(fileName: string) {
+    const fname = process.cwd() + '/' + fileName;
+    const re = /\/\*[\s\S]*?\*\/|(?<=[^:])\/\/.*|^\/\/.*/g
+
+    try {
+        let data = await promises.readFile(fileName, "utf8");
+        let contents = data.replace(re, "").replace(/\s*\r?\n/g, "\n");
+        await promises.writeFile(fname, contents)
+    } catch(err) {
+        onCancel(err as string);
+        return;
+    }
+}
+
+function updateValues(fileName: string, values: Map<String, any>) {
+    const fname = process.cwd() + '/' + fileName;
+    try {
+        let file = require(fname);
+        file = Object.assign(file, Object.fromEntries(values));
+        promises.writeFile(fname, JSON.stringify(file, null, 2))
+    } catch(err) {
+        onCancel(err as string);
+        return;
+    }
 }
 
 async function main() {
@@ -39,40 +100,46 @@ async function main() {
             title: 'Setting up npm package',
             task: async (message) => {
                 // Do installation here
-                myExecSync(`xxxnpm init -y`);
+                myExecSync(`npm init -init-version=0.0.1 -y && npm install ts-node && npm install --save-dev jest ts-jest @types/jest`).then(() => {
+                    updateValues('./package.json', new Map<string, any>([
+                        ['name', projectName],
+                        ['scripts', {test: "jest"}]
+                    ]));
+                });
                 return `Installed via npm`;
-          },
+            },
         },
         {
             title: 'Setting up typescript',
             task: async (message) => {
                 // Do installation here
-                // tsc --init
-                // update tsconfig.json with: "include": ["src/**/*", "tests/**/*"]
+                myExecSync(`tsc --init`).then(() => {
+                    stripComments('./tsconfig.json').then(() => {
+                        updateValues('./tsconfig.json', new Map<string, any>([
+                            ['include', ["src/**/*", "tests/**/*"]],
+                        ]));
+                    });
+                });
                 return 'Typescript initializer done';
-          },
+            },
         },
         {
             title: 'Creating src and test folders',
             task: async (message) => {
                 // Do installation here
-                // mkdir src 
-                // mkdir tests
+                promises.mkdir("src").then(() => {
+                    promises.writeFile("src/index.ts", sum);
+                });
+                promises.mkdir("tests").then(() => {
+                    promises.writeFile("tests/index.test.ts", sumTest);
+                });
 
-                // create .gitignore
-                // create /src/sum.ts
-                // create /tests/sum.test.ts
+                promises.writeFile(".gitignore", gitignore);
+                promises.writeFile("jest.config.ts", jestconfig);
 
-                // create /jest.config.ts
-
-                // npm install --save-dev jest
-                // npm install --save-dev ts-jest
-                // npm install --save-dev @types/jest
-
-                // update package.json with: "test": "jest"
                 return 'Source and test folders generated';
-          },
-        },
+            },
+        }
     ]);
 
     outro(`You're all set!`);
