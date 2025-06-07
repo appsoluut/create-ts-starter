2bd94c1162f90cff52f1af939fab21d0359ee9fb
Lieuwe Jansons <lieuwe.jansons@ns.nl>
2025-06-07 17:24:02 +0200

    Typescript configuration improvements. Added extra npm run commands, prefilled documentation and a bit of cleanup.

--- src/config/jest.ts
+++ src/config/jest.ts
@@ -1,8 +1,8 @@
-export const jestConfig =`module.exports = {
+export const jestConfig = `module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
-`
+`;
